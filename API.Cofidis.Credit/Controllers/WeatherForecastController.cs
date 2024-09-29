2b81bb0b5d5aaf5a5592940307ad48278de4ec47
markintela <markintela@hotmail.com>
2024-09-29 14:19:05 +0100

    Initial Commit

--- API.Cofidis.Credit/Controllers/WeatherForecastController.cs
+++ API.Cofidis.Credit/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace API.Cofidis.Credit.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecasts")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
